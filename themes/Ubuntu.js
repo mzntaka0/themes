05ae5ad2cdd9f79693c604fa479bf400c0ea0df0
Takao Mizuno <takao.mizuno110@gmail.com>
2019-02-06 00:44:09 +0900

    added Ubuntu theme

---
+++ themes/Ubuntu.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#2e3436", "#3465a4", "#4e9a06", "#06989a", "#cc0000", "#75507b", "#c4a000", "#d3d7cf", "#555753", "#729fcf", "#8ae234", "#34e2e2", "#ef2929", "#ad7fa8", "#fce94f", "#eeeeec"]);
+t.prefs_.set('foreground-color', "#4a4543");
+t.prefs_.set('background-color', "#300A24");
+t.prefs_.set('cursor-color', 'rgba(229,190,12,0.5)');
