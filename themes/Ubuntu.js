3be35cbaf4dae6d1446e3e4dd642cd9e15515828
Takao Mizuno <takao.mizuno110@gmail.com>
2019-02-06 00:49:15 +0900

    modified ubuntu theme

--- themes/Ubuntu.js
+++ themes/Ubuntu.js
@@ -1,4 +1,4 @@
-t.prefs_.set('color-palette-overrides',["#2e3436", "#3465a4", "#4e9a06", "#06989a", "#cc0000", "#75507b", "#c4a000", "#d3d7cf", "#555753", "#729fcf", "#8ae234", "#34e2e2", "#ef2929", "#ad7fa8", "#fce94f", "#eeeeec"]);
+t.prefs_.set('color-palette-overrides',["#222d3f", "#a82320", "#32a548", "#e58d11", "#3167ac", "#781aa0", "#2c9370", "#b0b6ba", "#212c3c", "#d4312e", "#2d9440", "#e5be0c", "#3c7dd2", "#8230a7", "#35b387", "#e7eced"]);
 t.prefs_.set('foreground-color', "#fff");
 t.prefs_.set('background-color', "#300A24");
 t.prefs_.set('cursor-color', 'rgba(229,190,12,0.5)');
